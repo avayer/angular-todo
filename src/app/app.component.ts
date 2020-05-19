c6165279cd7ce89a0fe7b07cfd06bf791daef05c
avayer <mahankalianurag@gmail.com>
2020-05-20 03:28:11 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+
+  @ViewChild('dataForm', { static: true }) form: ElementRef;
+  
+  noofCompleted = 0;
+  todoList: string[] = [
+    'Buy Veggies',
+    'Clean Lawn',
+    'Workout'
+  ];
+
+  completed: string[] = []
+
+  markCompleted(item: string, index: number) {
+    this.todoList.splice(index, 1);
+    this.completed.push(item);
+    this.noofCompleted = this.completed.length / (this.todoList.length + this.completed.length) * 100;
+  }
+
+  keyDownFunction(event, todoItem: HTMLInputElement) {
+    if (event.keyCode === 13) {
+      this.todoList.push(todoItem.value);
+      this.form.nativeElement.reset();
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
