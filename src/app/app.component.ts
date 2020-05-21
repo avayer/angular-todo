85b6371434c031870ee23ea36e638e2f3d6e00f0
avayer <mahankalianurag@gmail.com>
2020-05-21 19:19:31 +0530

    add editInPlace and darkMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
 
 
 @Component({
@@ -6,30 +8,85 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
 
+
+  color: ThemePalette = 'primary';
   @ViewChild('dataForm', { static: true }) form: ElementRef;
   
+  checked = false;
+  completed: string[] = []
   noofCompleted = 0;
-  todoList: string[] = [
-    'Buy Veggies',
-    'Clean Lawn',
-    'Workout'
+  todoList = [ 
+    { name: 'item1' },
+    { name: 'item2' },
+    { name: 'item3' },
+    { name: 'item4' }
   ];
 
-  completed: string[] = []
-
-  markCompleted(item: string, index: number) {
+  markCompleted(index, name) {
     this.todoList.splice(index, 1);
-    this.completed.push(item);
+    this.completed.push(name);
     this.noofCompleted = this.completed.length / (this.todoList.length + this.completed.length) * 100;
   }
 
   keyDownFunction(event, todoItem: HTMLInputElement) {
     if (event.keyCode === 13) {
-      this.todoList.push(todoItem.value);
+      this.todoList.push({name : todoItem.value});
       this.form.nativeElement.reset();
+      this.noofCompleted = this.completed.length / (this.todoList.length + this.completed.length) * 100;
     }
   }
+
+  deletePost(item, i) {
+    this.todoList.splice(i, 1);
+    console.log(this.todoList);
+    this.noofCompleted = this.completed.length / (this.todoList.length + this.completed.length) * 100;
+  }
+
+  getTheme() {
+    return this.checked ? '#121212' : 'white';
+  }
+
+  getColor() {
+    return this.checked ? 'white' : '';
+  }
+
+  controls: FormArray;
+
+  ngOnInit() {
+    const toGroups = this.todoList.map(entity => {
+
+      return new FormGroup({
+        name: new FormControl(entity.name, Validators.required),
+      });
+    });
+    this.controls = new FormArray(toGroups);
+
+  }
+
+  getControl(index: number, field: string): FormControl {
+    return this.controls.at(index).get(field) as FormControl;
+  }
+
+  updateField(index: number, field: string) {
+    console.log(index + ", " + field);
+    const control = this.getControl(index, field);
+
+    if (control.valid) {
+      this.todoList = this.todoList.map((e, i) => {
+        if (index === i) {
+          return {
+            ...e,
+            [field]: control.value
+          }
+        }
+        return e;
+      })
+    }
+    console.log(this.todoList);
+  }
+ 
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditableComponent } from './editable/editable.component';
+import { ViewModeDirective } from './editable/view-mode.directive';
+import { EditModeDirective } from './editable/edit-mode.directive';
+import { EditableOnEnterDirective } from './editable/edit-on-enter.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditableComponent,
+    ViewModeDirective,
+    EditModeDirective,
+    EditableOnEnterDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatListModule,
     MatCardModule,
     MatProgressBarModule,
     MatBadgeModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
